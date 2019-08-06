ce7b24099d1b34b450335bbcf9c6eeca2a3cc05c
Svetlana Kachkova <sv.kachkova@gmail.com>
2019-08-06 13:25:47 +0300

    SighUp and LogIn pages are function

--- src/Components/pages/LogInPage.tsx
+++ src/Components/pages/LogInPage.tsx
@@ -1,68 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Form, { State as UserData } from '../Form';
+import Form from '../Form';
 
-interface State {
-    isLoggin: boolean;
-    accessToken: string;
+interface Props {
+    handleSubmit: () => void;
 }
 
-class LogInPage extends React.Component<{}, State> {
-    constructor(props: {}) {
-        super(props);
-        this.state = {
-            isLoggin: false,
-            accessToken: ''
-        };
-
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-
-    handleSubmit(data: UserData) {
-        
-        const url: string = `http://192.168.1.2:3912/api/login?login=${data.login}&password=${data.password}`;
-        const options: RequestInit = {
-            method: 'GET',
-            mode: 'cors',
-        };
-    
-        fetch(url, options)
-            .then(response => {
-                if (response.ok) {
-                    return response.json();
-                }
-                throw new Error('Network response was not ok.');
-            })
-            .then(serverResponse => {
-                if (serverResponse.status) {
-                    this.setState({
-                       isLoggin: true,
-                       accessToken: serverResponse.token
-                    });    
-                } else {
-                    console.log(serverResponse.message);
-                }
-            })
-            .catch(error => console.log(error.message));
-    }
-    
-    render() {
-        return (
-            <div>
-                <h1>Log in</h1>
-                <p>
-                    Don't have an account
-                    ? <Link to='/signup'>
-                    Get started
-                    </Link>
-                </p>
-                <Form 
-                    buttonText='Login'
-                    handleSubmit={this.handleSubmit}
-                />
-            </div>
-        );
-    }
+function LogInPage(props: Props) {
+    return (
+        <div>
+            <h1>Log in</h1>
+            <p>
+                Don't have an account
+                ? <Link to='/signup'>
+                Get started
+                </Link>
+            </p>
+            <Form 
+                buttonValue='Login'
+                handleSubmit={() => props.handleSubmit()}
+            />
+        </div>
+    );
 };
 
 export default LogInPage;
--- src/Components/pages/SignUpPage.tsx
+++ src/Components/pages/SignUpPage.tsx
@@ -1,64 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Form, { State as UserData } from '../Form';
+import Form from '../Form';
 
-interface State {
-    userIsCreated: boolean;
+interface Props {
+    handleSubmit: () => void;
 }
 
-class SignUpPage extends React.Component<{}, State> {
-    constructor(props: {}) {
-        super(props);
-        this.state = {
-            userIsCreated: false
-        };
-
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-
-    handleSubmit(data: UserData) {
-        const url: string = `http://192.168.1.2:3912/api/createUser?login=${data.login}&password=${data.password}`;
-        const options: RequestInit = {
-            method: 'GET',
-            mode: 'cors',
-        };
-    
-        fetch(url, options)
-            .then(response => {
-                if (response.ok) {
-                    return response.json();
-                }
-                throw new Error('Network response was not ok.');
-            })
-            .then(serverResponse => {
-                if (serverResponse.status) {
-                    this.setState({
-                        userIsCreated: true
-                    });
-                } else {
-                    console.log(serverResponse.message);
-                }
-            })
-            .catch(error => console.log(error.message));
-    }
-
-    render() {
-        return (
-            <div>
-                <h1>Get started</h1>
-                <p>
-                    Already have an account
-                    ? <Link to='/loggin'>
-                        Login
-                    </Link>
-                </p>
-                <Form 
-                    buttonText='Join'
-                    handleSubmit={this.handleSubmit}
-                />
-            </div>
-        );
-    }
+function SignUpPage(props: Props) {
+    return (
+        <div>
+            <h1>Get started</h1>
+            <p>
+                Already have an account
+                ? <Link to='/loggin'>
+                    Login
+                </Link>
+            </p>
+            <Form 
+                buttonValue='Join'
+                handleSubmit={() => props.handleSubmit()}
+            />
+        </div>
+    );
 };
 
 export default SignUpPage;
